31331a25cc5befdc951f0678c5e1729dd3e1c049
forteproduccion <gustavo.aviles@forteproduccion.com>
2020-10-31 17:12:55 -0600

    CustomerPage
    Add Navigation Barr in Customer Page

--- Cotemar/Cotemar/Cotemar/ViewModels/Menu/CustomersPageViewModel.cs
+++ Cotemar/Cotemar/Cotemar/ViewModels/Menu/CustomersPageViewModel.cs
@@ -1,10 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Acr.UserDialogs;
+using Cotemar.Utils.Commands;
+using Cotemar.ViewModels.Bases;
+using Prism.Navigation;
+using System.Threading.Tasks;
 
 namespace Cotemar.ViewModels.Menu
 {
-    class CustomersPageViewModel
+    public class CustomersPageViewModel : PageViewModelBase
     {
+        #region Var Commands
+        public AsyncCommand NuevaMesa { get; set; }
+        #endregion
+        
+        #region Constructor
+        public CustomersPageViewModel(
+            INavigationService navigationService,
+            IUserDialogs userDialogsService) : base(navigationService, userDialogsService)
+        {
+        }
+        #endregion
+
+        #region Init Methods
+        protected override string Tag() => nameof(CustomersPageViewModel);
+
+        protected override string TitlePage() => "Menu";
+
+        #endregion
+
     }
+
+
 }
--- Cotemar/Cotemar/Cotemar/Views/Menu/CustomersPage.xaml.cs
+++ Cotemar/Cotemar/Cotemar/Views/Menu/CustomersPage.xaml.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Cotemar.Views.Bases;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Cotemar.Views.Menu
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class CustomersPage : ContentPage
+    public partial class CustomersPage : ContentPageBase
     {
         public CustomersPage()
         {
