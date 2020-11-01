15243f3ae3cfdffd31e2bd35769ca97edcb22261
landrygears3 <37632531+landrygears3@users.noreply.github.com>
2020-10-31 18:52:20 -0600

    Adds Visual Matelials
    Edit style entry
    adds visual materials logIn
    Adds visual materials itemcollectionUser

---
+++ Cotemar/Cotemar/Cotemar/RoundEffect.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace Cotemar
+{
+    class RoundEffect : RoutingEffect
+    {
+        public RoundEffect() : base($"Xamarin.{nameof(RoundEffect)}")
+        {
+        }
+    }
+}
