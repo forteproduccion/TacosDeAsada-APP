ac97f180c72600330573ac6c1c07013f178f34b7
landrygears3 <37632531+landrygears3@users.noreply.github.com>
2020-11-06 03:26:39 -0600

    UpdateModels

---
+++ Cotemar/Cotemar/Cotemar/Models/Spartane/ProductsModel.cs
@@ -0,0 +1,25 @@
+﻿/*
+ Id => 1 quesadilla, 2 taco, 3 otro
+ */
+using System.Drawing;
+
+namespace Cotemar.Models.Spartane
+{
+    public class ProductsModel
+    {
+        public long Id{ get; set;}
+        public string Name { get; set; }
+        public Type SelectProduct { get; set; }
+        public double Price { get; set; }
+        public Color BgColor { get; set; }
+    }
+    public partial class Type
+    { 
+        /*
+         1 = carne, 2=otro
+         */
+        public long Id { get; set; }
+        public string Name { get; set; }
+    
+    }
+}
--- Cotemar/Cotemar/Cotemar/Models/Spartane/TableModel.cs
+++ Cotemar/Cotemar/Cotemar/Models/Spartane/TableModel.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿
+using System.Drawing;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Cotemar.Models.Spartane
 {
-    class TableModel
+    public class TableModel
     {
+        public long Id { get; set; }
+        public double Current { get; set; }
+        public List<Customer> Customer{get; set;}
+        public Color BgColor { get; set; }
+
+    }
+    public partial class Customer {
+
+        public long Id { get; set; }
+        public double Current { get; set; }
+        public List<Product> Products{ get; set; }
+
+        public Color BgColor { get; set; }
+
+    }
+
+    public partial class Product {
+        public ProductsModel Type{get; set;}
+        public int Quantity { get; set; }
     }
 }
